fda2cad247623496ac2d05dd596406e890d97869
Abhi-109 <abhimalnad9@gmail.com>
2026-01-01 23:14:06 +0530

    Add query to classify customers as one-time or repeat buyers based on delivered orders

--- queries/02_customer_analysis.sql
+++ queries/02_customer_analysis.sql
@@ -14,3 +14,16 @@ LIMIT 10;
 
 
 
+-- Classify customers into one-time and repeat buyers based on delivered orders
+SELECT
+    c.customer_id,
+    COUNT(DISTINCT o.order_id) AS order_count,
+    CASE
+        WHEN COUNT(DISTINCT o.order_id) = 1 THEN 'one_time'
+        ELSE 'repeat'
+    END AS customer_type
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+WHERE o.order_status = 'delivered'
+GROUP BY c.customer_id;
